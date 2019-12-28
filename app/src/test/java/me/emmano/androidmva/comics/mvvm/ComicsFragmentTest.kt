df4d74783adca7e4083bbe489d5b00fb915a8876
Emmanuel Ortiguela <emmanuel.ortiguela@asynchrony.com>
2019-12-28 11:50:17 -0600

    adapter2 - Update dependencies and update simple fragment test. ()

--- app/src/test/java/me/emmano/androidmva/comics/mvvm/ComicsFragmentTest.kt
+++ app/src/test/java/me/emmano/androidmva/comics/mvvm/ComicsFragmentTest.kt
@@ -2,17 +2,22 @@ package me.emmano.androidmva.comics.mvvm
 
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import me.emmano.androidmva.CoroutineTest
+import me.emmano.androidmva.R
 import me.emmano.androidmva.base.RobolectricTest
 import me.emmano.androidmva.rule.CoroutineTestRule
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.koin.core.inject
-
 @ExperimentalCoroutinesApi
 class ComicsFragmentTest : RobolectricTest(), CoroutineTest {
 
@@ -20,16 +25,27 @@ class ComicsFragmentTest : RobolectricTest(), CoroutineTest {
     override val coroutineRule: CoroutineTestRule = CoroutineTestRule()
 
     private val viewModel: ComicsViewModel by inject()
+    private val comics = MutableLiveData<List<ComicModel>>()
 
     @Before
     fun setUp() {
-        declareViewModelMock<ComicsViewModel> { on { comics } doReturn MutableLiveData() }
-
-        launchFragmentInContainer<ComicsFragment>()
+        declareViewModelMock<ComicsViewModel> { on { comics } doReturn comics }
     }
 
     @Test
-    fun `onViewCreated loads comics`() = test {
+    fun `onViewCreated loads and displays comics`() = test {
+        val comicCell = ComicCell("title", "description", "imageUrl")
+        launchFragmentInContainer<ComicsFragment>()
+
         verify(viewModel).loadComics()
+
+        comics.value = listOf(comicCell)
+
+        onView(withId(R.id.comics))
+            .perform(
+                RecyclerViewActions.actionOnItem<RecyclerView.ViewHolder>(
+                hasDescendant(withText(comicCell.title)),
+                click()))
+
     }
 }
