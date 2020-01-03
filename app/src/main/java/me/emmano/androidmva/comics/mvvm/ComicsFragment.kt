b862594ddaa3344ead2403647274e51cb54b3e90
Emmanuel Ortiguela <emmanuel.ortiguela@asynchrony.com>
2020-01-03 09:35:57 -0600

    coroutine-store - Fix unit test. Move loadComics call back to onViewCreated. (EO)

--- app/src/main/java/me/emmano/androidmva/comics/mvvm/ComicsFragment.kt
+++ app/src/main/java/me/emmano/androidmva/comics/mvvm/ComicsFragment.kt
@@ -2,6 +2,7 @@ package me.emmano.androidmva.comics.mvvm
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -32,12 +33,13 @@ class ComicsFragment : Fragment() {
                }
            }.root
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.loadComics()
+    }
+
     override fun onResume() {
         super.onResume()
-        lifecycleScope.launch {
-            viewModel.loadComics()
-        }
-
         viewModel.comics.observe(this, Observer {
             adapter.submitList(it)
         })
