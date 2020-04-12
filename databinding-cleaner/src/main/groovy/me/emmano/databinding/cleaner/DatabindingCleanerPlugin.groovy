2d81437d6507d533692cfe7e3d3cee1cda620809
Wayfinder <emmanuel.ortiguela@asynchrony.com>
2020-04-11 23:28:43 -0500

    databinding-cleaner - Create plugin to remove memory leaks caused by retaining views inside bindings. (EO)

---
+++ databinding-cleaner/src/main/groovy/me/emmano/databinding/cleaner/DatabindingCleanerPlugin.groovy
@@ -0,0 +1,11 @@
+package me.emmano.databinding.cleaner
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+class DatabindingCleanerPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        project.gradle.taskGraph.addTaskExecutionGraphListener new DatabindingTaskListener()
+    }
+}
